d0902340938fc3bbbe9d9c0186c39c62eee160ce
nikhil0441 <nikhilpatel03022004@gmail.com>
2025-09-03 13:16:03 +0530

    added all files

---
+++ src/test/java/com/web/saree/SareeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.saree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SareeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
