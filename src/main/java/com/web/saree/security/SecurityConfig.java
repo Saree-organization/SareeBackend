6db2d89e1ff025b263149204b4dc5d836416ae77
nikhil0441 <nikhilpatel03022004@gmail.com>
2025-09-10 10:29:53 +0530

    remove twilio and added console otp

--- src/main/java/com/web/saree/security/SecurityConfig.java
+++ src/main/java/com/web/saree/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.web.saree.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -40,7 +41,7 @@ public class SecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/**").permitAll() // This single line is enough to permit all requests
                         .anyRequest().authenticated()
                 );
 
